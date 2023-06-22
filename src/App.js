149235aa4b09376460ee0d6ec69b70facb0b43c8
CodeForRevolution <121334156+CodeForRevolution@users.noreply.github.com>
2023-06-22 14:57:32 +0530

    working to solve

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   
   const router=createBrowserRouter([
     { 
-      path:'/' ,element:<NavBar/> ,children:[
+      path:'' ,element:<NavBar/> ,children:[
         {index:true,element:<Home/>},
         {path:'todo',element:<TodoForm/>},
         {path:'notes',element:<NoteForm/>}
